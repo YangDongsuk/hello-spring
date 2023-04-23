d7d47ff53d200d7f0e6e1003f9cddc6cd40c5d3d
dongsuk <scienceyds@naver.com>
2023-04-23 22:54:56 +0900

    회원 관리 예제-백엔드 개발

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -39,7 +39,7 @@ public class HelloController {
     static class Hello{
         private String name;
 
-        public String getName(){ 
+        public String getName(){
             return name;
         }
 
